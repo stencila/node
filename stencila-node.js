faf9e347ca59bac50dbd540437978c2a5f851725
Nokome Bentley <nokome@stenci.la>
2016-10-27 12:05:59 +1300

    Use promise

--- stencila-node.js
+++ stencila-node.js
@@ -7,6 +7,6 @@ const colors = require('colors') // eslint-disable-line no-unused-vars
 console.log(('Stencila CLI for Node.js ' + stencila.version + '\n').green)
 
 // Start up the host and notify user
-stencila.host.startup()
-console.log(stencila.host.title + ' is being served at ' + stencila.host.url.blue)
-
+stencila.host.startup().then(function () {
+  console.log(stencila.host.title + ' is being served at ' + stencila.host.url)
+})
