4a55d410e45aafd88fcbce421e8e1ca8e3362582
Nokome Bentley <nokome@stenci.la>
2016-11-29 08:57:33 +1300

    Fix ComponentProxy test

--- tests/component/ComponentProxy.test.js
+++ tests/component/ComponentProxy.test.js
@@ -12,7 +12,7 @@ host.serve().then(() => {
   })
 
   test('ComponentProxy can get a property', function (t) {
-    let c = new ComponentProxy('document', host.url + '/+document')
+    let c = new ComponentProxy('document', null, null, host.url + '/+document')
     c.get('type')
       .then(function (value) {
         t.equal(value, 'document')
@@ -25,7 +25,7 @@ host.serve().then(() => {
   })
 
   test('ComponentProxy can set a property', function (t) {
-    let c = new ComponentProxy('document', host.url + '/+document')
+    let c = new ComponentProxy('document', null, null, host.url + '/+document')
     c.set('html', '<p>Hello from Node.js</p>')
       .then(function () {
         t.end()
@@ -37,7 +37,7 @@ host.serve().then(() => {
   })
 
   test.skip('ComponentProxy can call a method', function (t) {
-    let c = new ComponentProxy('document', host.url + '/+jssession')
+    let c = new ComponentProxy('document', null, null, host.url + '/+jssession')
     c.call('print', '6*7')
       .then(function (value) {
         t.equal(value, '42')
