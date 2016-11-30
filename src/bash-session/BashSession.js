4a45a5aea54fa5309945653f49297887de5d0fc0
Nokome Bentley <nokome@stenci.la>
2016-11-30 14:29:02 +1300

    Add BashSession.
    A very preliminary implementation

---
+++ src/bash-session/BashSession.js
@@ -0,0 +1,36 @@
+const Session = require('../session/Session')
+
+const spawn = require('child_process').spawn
+
+class BashSession extends Session {
+
+  constructor () {
+    super()
+    this._bash = spawn('bash')
+  }
+
+  get type () {
+    return 'bash-session'
+  }
+
+  execute (code) {
+    let result = {
+      errors: null,
+      output: null,
+      pipes: null
+    }
+    return new Promise((resolve, reject) => {
+      this._bash.stdout.on('data', data => {
+        result.output = {
+          format: 'text',
+          content: data.toString('utf8')
+        }
+        resolve(result)
+      })
+      this._bash.stdin.write(code + '\n')
+    })
+  }
+
+}
+
+module.exports = BashSession
