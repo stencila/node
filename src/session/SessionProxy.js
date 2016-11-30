38b605a4d9ed4331f8f7e29f82e2323b8adf6729
Nokome Bentley <nokome@stenci.la>
2016-11-30 14:28:41 +1300

    Add methods to SessionProxy

--- src/session/SessionProxy.js
+++ src/session/SessionProxy.js
@@ -2,6 +2,14 @@ const ComponentProxy = require('../component/ComponentProxy')
 
 class SessionProxy extends ComponentProxy {
 
+  execute (code) {
+    return this.call('execute', code)
+  }
+
+  print (expr) {
+    return this.call('print', expr)
+  }
+
 }
 
 module.exports = SessionProxy
