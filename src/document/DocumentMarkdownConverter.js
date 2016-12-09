d97b71794b0646d2eb11e95dcb89591b85e6f778
Nokome Bentley <nokome@stenci.la>
2016-12-09 14:52:48 +1300

    Use `commonmark` specification for Pandoc `md` format conversion

--- src/document/DocumentMarkdownConverter.js
+++ src/document/DocumentMarkdownConverter.js
@@ -5,14 +5,16 @@ class DocumentMarkdownConverter extends ComponentConverter {
 
   load (document, content, format, options) {
     options = options || {}
-    let html = pandoc.convert(content, 'markdown', 'html')
+    let html = pandoc.convert(content, 'commonmark', 'html', {
+      'no-highlight': null
+    })
     document.load(html, 'html')
   }
 
   dump (document, format, options) {
     options = options || {}
     let html = document.dump('html')
-    return pandoc.convert(html, 'html', 'markdown', {
+    return pandoc.convert(html, 'html', 'commonmark', {
       'wrap': options.wrap || 'preserve',
       'columns': options.columns || 100
     })
