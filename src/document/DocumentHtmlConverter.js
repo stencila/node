848759e1c87cbea568ad10e7459f3606902f7073
Nokome Bentley <nokome@stenci.la>
2017-01-06 16:09:06 +1300

    Don't wrap HTML

--- src/document/DocumentHtmlConverter.js
+++ src/document/DocumentHtmlConverter.js
@@ -30,7 +30,32 @@ class DocumentHtmlConverter extends ComponentConverter {
 
     let html = document.content.html()
     // See beautification options at https://github.com/beautify-web/js-beautify/blob/master/js/lib/beautify-html.js
-    html = beautify.html(html)
+    //   indent_inner_html (default false)  — indent <head> and <body> sections,
+    //   indent_size (default 4)          — indentation size,
+    //   indent_char (default space)      — character to indent with,
+    //   wrap_line_length (default 250)            -  maximum amount of characters per line (0 = disable)
+    //   brace_style (default "collapse") - "collapse" | "expand" | "end-expand" | "none"
+    //           put braces on the same line as control statements (default), or put braces on own line (Allman / ANSI style), or just put end braces on own line, or attempt to keep them where they are.
+    //   unformatted (defaults to inline tags) - list of tags, that shouldn't be reformatted
+    //   content_unformatted (defaults to pre tag) - list of tags, that its content shouldn't be reformatted
+    //   indent_scripts (default normal)  - "keep"|"separate"|"normal"
+    //   preserve_newlines (default true) - whether existing line breaks before elements should be preserved
+    //                                       Only works before elements, not inside tags or for text.
+    //   max_preserve_newlines (default unlimited) - maximum number of line breaks to be preserved in one chunk
+    //   indent_handlebars (default false) - format and indent {{#foo}} and {{/foo}}
+    //   end_with_newline (false)          - end with a newline
+    //   extra_liners (default [head,body,/html]) -List of tags that should have an extra newline before them.
+    html = beautify.html(html, {
+      'indent_inner_html': false,
+      'indent_size': 2,
+      'indent_char': ' ',
+      'wrap_line_length': 0, // disable wrapping
+      'brace_style': 'expand',
+      'preserve_newlines': true,
+      'max_preserve_newlines': 5,
+      'indent_handlebars': false,
+      'extra_liners': ['/html']
+    })
     // Create a standalone HTML document?
     if (options.standalone) {
       let theme = options.theme || 'https://unpkg.com/stencila-web/build/document.min'
