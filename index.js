72ba57b3b0a1f5a356c7eb1fe0ab4df07b59d469
Nokome Bentley <nokome@stenci.la>
2017-03-15 12:35:18 +1300

    Remove cruft from index

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-/*
- * Primary entry point for this package. Only exports
- * classes and objects that will normally be used by external
- * packages. Base classes (e.g. `Component`) and internal classes
- * (e.g. `Host`) are not exported
- */
-
-const Document = require('./src/document/Document')
-const Sheet = require('./src/sheet/Sheet')
-const JsSession = require('./src/js-session/JsSession')
 const host = require('./src/host/host')
 const version = require('./package').version
 
 module.exports = {
-  Document: Document,
-  Sheet: Sheet,
-  JsSession: JsSession,
   host: host,
   version: version
 }
