1d42820fa9e1fc293502e6fbed643dfbb4fa4f68
Nokome Bentley <nokome@stenci.la>
2017-03-18 19:15:43 +1300

    Disable linting

--- stencila-node.js
+++ stencila-node.js
@@ -4,9 +4,9 @@ const stencila = require('.')
 const colors = require('colors') // eslint-disable-line no-unused-vars
 
 // Banner
-console.log(('Stencila CLI for Node.js ' + stencila.version + '\n').green)
+console.log(('Stencila CLI for Node.js ' + stencila.version + '\n').green) // eslint-disable-line no-console
 
 // Start up the host and notify user
 stencila.host.start().then(function () {
-  console.log('Stencila host is being served at ' + stencila.host._servers.http.url)
+  console.log('Stencila host is being served at ' + stencila.host._servers.http.url) // eslint-disable-line no-console
 })
