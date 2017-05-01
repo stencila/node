34aa7e62d0182553111d921084d9f2449f56a10e
Nokome Bentley <nokome@stenci.la>
2017-04-30 18:18:17 -0700

    Remove unecessary dependency

--- stencila-node.js
+++ stencila-node.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 
 const stencila = require('.')
-const colors = require('colors') // eslint-disable-line no-unused-vars
 
 // Banner
-console.log(('Stencila CLI for Node.js ' + stencila.version + '\n').green) // eslint-disable-line no-console
+console.log(('Stencila CLI for Node.js ' + stencila.version + '\n')) // eslint-disable-line no-console
 
 // Start up the host and notify user
 stencila.host.start().then(function () {
