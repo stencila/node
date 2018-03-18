9fcf0c329500a5b362ead49140a7c441bd9d9511
Nokome Bentley <nokome@stenci.la>
2018-03-19 12:47:37 +1300

    Remove require

--- test/contexts/NodeContext.test.js
+++ test/contexts/NodeContext.test.js
@@ -1,5 +1,4 @@
 const test = require('tape')
-const {pack} = require('stencila').value
 
 const NodeContext = require('../../lib/contexts/NodeContext')
 
@@ -14,12 +13,12 @@ test.skip('NodeContext', function (t) {
     .then(() => {
       c.runCode('foo + "t_simpson"')
         .then(result => {
-          t.deepEqual(result, {errors: null, output: pack('bart_simpson')})
+          t.deepEqual(result, {errors: null, output: c.pack('bart_simpson')})
         })
     })
 
-  c.callCode('return a*6', {a: pack(7)}).then(result => {
-    t.deepEqual(result, {errors: null, output: pack(42)})
+  c.callCode('return a*6', {a: c.pack(7)}).then(result => {
+    t.deepEqual(result, {errors: null, output: c.pack(42)})
   })
 
   c.codeDependencies('foo').then(result => t.deepEqual(result, ['foo']))
