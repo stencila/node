42f02857a6c7d4b2078b753017647fd3abefe851
Nokome Bentley <nokome@stenci.la>
2018-03-26 12:04:31 +1300

    Add test for inter-machine pointers

--- test/contexts/Context.test.js
+++ test/contexts/Context.test.js
@@ -18,6 +18,12 @@ test('Context.packPointer+unpackPointer', async assert => {
 
   const pointerA1X = await contextA1.packPointer('string', 'A1X')
   const pointerB1X = await contextB1.packPointer('string', 'B1X')
+  // Simulate a pointer to data on another machine
+  const pointerC1X = Object.assign({}, pointerB1X)
+  pointerC1X.machine = {
+    mac: 'Some other MAC',
+    ip: 'Some other IP'
+  }
 
   assert.deepEqual(pointerA1X, {
     type: 'string',
@@ -36,11 +42,20 @@ test('Context.packPointer+unpackPointer', async assert => {
     }
   })
 
-  assert.deepEqual(await contextA1.unpackPointer(pointerA1X), 'a1x', 'Acessible from same context')
-  assert.deepEqual(await contextA2.unpackPointer(pointerA1X), 'a1x', 'Acessible from another context on host')
-  assert.deepEqual(await contextB1.unpackPointer(pointerA1X), 'a1x', 'Acessible from another context on another host')
+  assert.deepEqual(await contextA1.unpackPointer(pointerA1X), 'a1x', 'Accessible from same context')
+  assert.deepEqual(await contextA2.unpackPointer(pointerA1X), 'a1x', 'Accessible from another context on host')
+  assert.deepEqual(await contextB1.unpackPointer(pointerA1X), 'a1x', 'Accessible from another context on another host')
 
-  assert.deepEqual(await contextA1.unpackPointer(pointerB1X), 'b1x', 'Acessible from same context')
+  assert.deepEqual(await contextA1.unpackPointer(pointerB1X), 'b1x', 'Accessible from another context on another host started before')
+
+  async function tryCatch () {
+    try {
+      await contextA1.unpackPointer(pointerC1X)
+    } catch (error) {
+      return error.message
+    }
+  }
+  assert.equal(await tryCatch(), 'Inter-machine pointers are not yet supported')
 
   await hostA.stop()
   await hostB.stop()
