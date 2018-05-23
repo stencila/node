dbba326033287e9faa5624bf7b6045f1fb6d1db9
Nokome Bentley <nokome@stenci.la>
2018-05-23 16:21:33 +1200

    Linting

--- test/contexts/JupyterContext.test.js
+++ test/contexts/JupyterContext.test.js
@@ -4,14 +4,14 @@ const JupyterContext = require('../../lib/contexts/JupyterContext')
 
 testAsync('JupyterContext', async assert => {
   await JupyterContext.discover()
-  
+
   // These tests can only be run if at least one Jupyter kernel is installed
   assert.pass('JupyterContext.spec.kernels: ' + JSON.stringify(Object.keys(JupyterContext.spec.kernels)))
   if (Object.keys(JupyterContext.spec.kernels).length < 1) {
     assert.end()
     return
   }
-    
+
   let context = new JupyterContext({
     language: 'python',
     debug: true,
@@ -19,7 +19,7 @@ testAsync('JupyterContext', async assert => {
   })
 
   assert.pass('JupyterContext.kernel: ' + context.kernel)
-  
+
   await context.initialize()
   assert.pass('JupyterContext._config: ' + JSON.stringify(context._config))
   assert.pass('JupyterContext._kernelInfo: ' + JSON.stringify(context._kernelInfo))
